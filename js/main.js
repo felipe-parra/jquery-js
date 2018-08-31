cbf33849ba2f90492e3fa6097d8b25cdbcfb71f8
Phick <fpr29.10@gmail.com>
2018-08-31 03:11:27 -0500

    Avanzando con JavaScript

--- js/main.js
+++ js/main.js
@@ -5,24 +5,34 @@ const urlApi = "https://yts.am/api/v2/list_movies.json?genre=";
 let genre = ['action', 'drama','animation'];
 
 (async function load(){
-    async function getData(url,genero){
+    async function getData(url, genero){
         const urlComplete = url + genero;
         const response = await fetch(urlComplete);
         const data = await response.json();
         return data;
 
     }
+    const $form = document.getElementById("form");
+    $form.addEventListener("submit", (event) => {
+      debugger
+    })
     const actionList = await getData(urlApi,'action');
+    const biographyList = await getData(urlApi,'biography');
+    const dramaList = await getData(urlApi,'drama');
+
     const $actionContainer = document.getElementById('action');
+    const $biographyContainer = document.getElementById('biography');
+    const $dramaContainer = document.getElementById('drama');
+
     function videoItemTemplate(movie){
         return (
             `<div class="row-fluid">
-                <div class="col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-3">
                     <div class="thumbnail" id="action">
                         <img src="${movie.medium_cover_image}" class="img-thumbnail" alt="">
                         <div class="caption">
                             <h3>${movie.title}</h3>
-                            <p>${movie.description_full}</p>
+                            <p>${movie.description_full.slice(0,140)}</p>
                             <p>
                                 <a href="#" class="btn btn-primary" role="button">Ver mas</a>
                                 <a href="#" class="btn btn-default" role="button">Download
@@ -38,17 +48,28 @@ let genre = ['action', 'drama','animation'];
         html.body.innerHTML = HTMLString;
         return html.body.children[0];
     }
-    //  await
-    //console.log(actionList);
+    function addEventClick($element){
+      $element.addEventListener("click", function(){
+        alert("click")
+      })
+    }
     function renderMovieList(list, $container){
-        //actionList.data.movies
-        $container.children[0].remove();
+        $container.querySelector("#loader").remove();
         list.forEach((movie) => {
             const HTMLString = videoItemTemplate(movie);
             const movieElement = createTemplate(HTMLString);
-            $actionContainer.append(movieElement);
+            $container.append(movieElement);
+            addEventClick(movieElement);
         })
     }
     renderMovieList(actionList.data.movies,$actionContainer)
+    renderMovieList(biographyList.data.movies,$biographyContainer)
+    renderMovieList(dramaList.data.movies,$dramaContainer)
+
+    function selectedDropdown(){
+      let selectedItem = document.getElementById("selectGenre").value;
+      const actionList = getData(urlApi,selectedItem);
+      renderMovieList(actionList.data.movies,$actionContainer)
+    }
 
 })()
