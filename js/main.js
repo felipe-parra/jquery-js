70633bfeabd01ba75660ff42592b081fefd23d63
phick-ws <fpr29.10@gmail.com>
2018-08-31 00:14:17 -0500

    Avance con bootstrap

--- js/main.js
+++ js/main.js
@@ -5,14 +5,50 @@ const urlApi = "https://yts.am/api/v2/list_movies.json?genre=";
 let genre = ['action', 'drama','animation'];
 
 (async function load(){
-    //  await
     async function getData(url,genero){
-        let urlComplete = url + genero;
-        let response = await fetch(urlComplete);
-        let data = await response.json();
-        console.log(data);
-    }
-    getData(urlApi,genre[2])
-})()
+        const urlComplete = url + genero;
+        const response = await fetch(urlComplete);
+        const data = await response.json();
+        return data;
 
-const $home = document.getElementById('modal')
+    }
+    const actionList = await getData(urlApi,'action');
+    const $actionContainer = document.getElementById('action');
+    function videoItemTemplate(movie){
+        return (
+            `<div class="row-fluid">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" id="action">
+                        <img src="${movie.medium_cover_image}" class="img-thumbnail" alt="">
+                        <div class="caption">
+                            <h3>${movie.title}</h3>
+                            <p>${movie.description_full}</p>
+                            <p>
+                                <a href="#" class="btn btn-primary" role="button">Ver mas</a>
+                                <a href="#" class="btn btn-default" role="button">Download
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>`
+        )
+    }
+    function createTemplate(HTMLString){
+        const html = document.implementation.createHTMLDocument();
+        html.body.innerHTML = HTMLString;
+        return html.body.children[0];
+    }
+    //  await
+    //console.log(actionList);
+    function renderMovieList(list, $container){
+        //actionList.data.movies
+        $container.children[0].remove();
+        list.forEach((movie) => {
+            const HTMLString = videoItemTemplate(movie);
+            const movieElement = createTemplate(HTMLString);
+            $actionContainer.append(movieElement);
+        })
+    }
+    renderMovieList(actionList.data.movies,$actionContainer)
+
+})()
