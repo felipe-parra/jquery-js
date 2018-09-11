fd89e0b55652b78fce851c8cca0785912391ea73
phick-ws <fpr29.10@gmail.com>
2018-09-11 15:32:34 -0500

    Searching adding

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //  Archivo JS definido por phick.
 
-const urlApi = "https://yts.am/api/v2/list_movies.json?genre=";
+const urlApi = "https://yts.am/api/v2/list_movies.json?limit=4&genre=";
+const BASE_API = "https://yts.am/api/v2/list_movies.json?";
+
 
 let genre = ['action', 'drama','animation'];
 
@@ -10,12 +12,19 @@ let genre = ['action', 'drama','animation'];
         const response = await fetch(urlComplete);
         const data = await response.json();
         return data;
-
+    }
+    async function getMovieSearch(url){
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
     }
     const $form = document.getElementById("form");
-    $form.addEventListener("submit", (event) => {
-      debugger
-    })
+    const $featuring = document.getElementById("featuring");
+    function setAttributes($element, attributes){
+        for (const attribute in attributes){
+            $element.setAttribute(attribute,attributes[attribute]);
+        }
+    };
     const actionList = await getData(urlApi,'action');
     const biographyList = await getData(urlApi,'biography');
     const dramaList = await getData(urlApi,'drama');
@@ -23,19 +32,53 @@ let genre = ['action', 'drama','animation'];
     const $actionContainer = document.getElementById('action');
     const $biographyContainer = document.getElementById('biography');
     const $dramaContainer = document.getElementById('drama');
-
+    function resultTemplate(dataMovie){
+        return (
+            `<div class="home-featuring alert alert-success alert-dismissible fade in" id="featuring" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <div class="media">
+                    <div class="media-left">
+                        <img id="imgResultado" src="${dataMovie.medium_cover_image}" alt="">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">${dataMovie.title}</h4>
+                        <p>${dataMovie.description_full}</p>
+                    </div>
+                </div>
+            </div>`
+        )
+    }
+    function failResult(){
+        return (
+            `<div class="home-featuring alert alert-danger alert-dismissible fade in" id="featuring" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+                <div class="media">
+                    <strong>Sorry, not find the movie.</strong>
+                    <div class="media-left">
+                        <img id="imgResultado" src="img/error.jpg" alt="">
+                    </div>
+                    <div class="media-body">
+                    </div>
+                </div>
+            </div>`
+        )
+    }
     function videoItemTemplate(movie){
         return (
             `<div class="row-fluid">
-                <div class="col-sm-6 col-md-3">
+                <div class="col-sm-4 col-md-3">
                     <div class="thumbnail" id="action">
                         <img src="${movie.medium_cover_image}" class="img-thumbnail" alt="">
                         <div class="caption">
-                            <h3>${movie.title}</h3>
+                            <h4>${movie.title}</h4>
                             <p>${movie.description_full.slice(0,140)}</p>
                             <p>
-                                <a href="#" class="btn btn-primary" role="button">Ver mas</a>
-                                <a href="#" class="btn btn-default" role="button">Download
+                                <a href="#" class="btn btn-primary btn-sm"  data-toggle="modal" data-target="#myModal" role="button">Ver mas</a>
+                                <a href="#" class="btn btn-default btn-sm" role="button">Download
                                 </a>
                             </p>
                         </div>
@@ -48,10 +91,34 @@ let genre = ['action', 'drama','animation'];
         html.body.innerHTML = HTMLString;
         return html.body.children[0];
     }
+    const $modal = document.getElementById('myModal');
+    const $modalTitle = document.getElementById('myModalLabel');
+    const $modalBody = document.getElementsByClassName('modal-body');
+
+    const $modalBodyTitle = document.querySelector('h4');
+    const $modalImage = document.querySelector('img');
+    const $modalDescription = document.querySelector('p');
+
+    $form.addEventListener("submit", async (event) => {
+        event.preventDefault(); //Para evitar que el navegador haga refresh al enviar submit
+        //$featuring.classList.add("search-active");
+        // $featuring.append($loader);
+        const data = new FormData($form);
+        const dataMovie = await getMovieSearch(`${BASE_API}limit=1&query_term=${data.get('movieSearch')}`);
+
+        if (dataMovie.data.movie_count <= 0) {
+            const HTMLString = failResult();
+            $featuring.innerHTML = HTMLString;
+            } else{
+                const HTMLString = resultTemplate(dataMovie.data.movies[0]);
+                $featuring.innerHTML = HTMLString;
+            }
+    })
+    function mostrarModal(){
+
+    }
     function addEventClick($element){
-      $element.addEventListener("click", function(){
-        alert("click")
-      })
+
     }
     function renderMovieList(list, $container){
         $container.querySelector("#loader").remove();
